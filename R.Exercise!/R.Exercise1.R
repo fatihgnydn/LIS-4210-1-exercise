bd907964e9527f20cbe5c8d98d6f3827c60458e1
fatihgnydn <49775306+fatihgnydn@users.noreply.github.com>
2019-04-22 00:39:07 -0600

    Add files via upload

---
+++ R.Exercise!/R.Exercise1.R
@@ -0,0 +1,100 @@
+install.packages("stringr", repos='http://cran.us.r-project.org')
+library(stringr)
+library(tidyverse)
+#Part 1. Introductions
+my.age <- 29
+my.age
+my.name <- "Fatih"
+my.name
+MakeIntroduction <- function(name, age) {paste("Hello, my name is", name, ", and I'm ", age, "years old!")}
+function()
+  re\
+#Create a variable casual
+casual.intro <- sub("Hello, my name is", "Hey, I'm", my.intro)
+my.intro <- make_birthday_intro(my.name, my.age)
+my.intro <- make_birthday_intro(my.name, my.age)
+my.intro <- make_birthday_intro
+my.intro <- "make_birthday_intro"
+my.intro <- "make_birthday_intro"(my.name,my.age)
+MakeIntroduction <- function(name, age)
+{paste("Hello, my name is", name, ", and I'm ", age, "years old!")}
+my.intro <- make_birthday_intro(my.name, my.age)
+MakeIntroduction <- function(name, age)
+{paste("Hello, my name is", my.name, ", and I'm ", my.age, "years old!")}
+my.intro <- MakeIntroduction(my.name, my.age)
+my.intro
+#Create a variable casual
+casual.intro <- sub("Hello, my name is", "Hey, I'm", my.intro)
+print(casual.intro)
+#Create variable capital.intro
+capital.intro <- str_to_upper(casual.intro)
+capital.intro
+capital.intro <- str_to_title(casual.intro)
+capital.intro
+#Create a variable intro.e.count that stores the number of times the letter
+intro.e.count <- str_count(my.intro, pattern = 'e')
+print(intro.e.count)
+#Part 2: BOOKS
+#Create variable that is vector of 6 books
+books <- c("The Kite Runner", "A Thousand Splendid Suns", "Lord of the Rings", "Improbable", "The alchemist")
+books
+#Create variable top.three.books
+top.three.books<- book[c(1:3)]
+top.three.books<- books[c(1:3)]
+top.three.books
+#Create varaible book.reviews
+book.reviews<- "is a great read!"
+book.reviews
+book.reviews <- paste(book, "is a great read!")
+book.reviews <- paste(books, "is a great read!")
+book.reviews
+books.without.four <- books[c(1:3,5:6)]
+books.without.four
+books.without.four <- books[c(-4)]
+books.without.four
+#Create variable long.titles
+long.titles <- book[nchar(book) > 15]
+long.titles <- books[nchar(books) > 15]
+long.titles
+long.titles <- str_count(books) > 15
+long.titles
+#Part 3: Squares
+numbers <- (1:201)
+numbers
+#Create a variable squared.numbers that is your numbers vector with all of the values squared
+squared.numbers <- numbers^2
+print(squared.numbers)
+#Create a variable squared.mean that contains the mean (average) value of the squared vector.
+squared.mean <- mean(squared.numbers)
+print(squared.mean)
+#Create variable of whole number squares.
+squares <- list()
+for(which.numbers in numbers){
+  if(sqrt(??)==round(sqrt(??))){
+    squares <- c(??, ??)
+  }
+}
+squares <- list(numbers) for(which.numbers in numbers){
+  if(sqrt(squa)==round(sqrt(numbers))){
+    squares <- c(1, 201)
+  }
+}
+squares <- list(numbers)
+for(which.numbers in numbers){
+  if(sqrt(squared.numbers)==round(sqrt(numbers))){
+    squares <- c(1, 201)
+  }
+}
+squares <- list(numbers)
+for(which.numbers in numbers){
+  +     if(sqrt(squared.numbers)==round(sqrt(numbers))){
+    +         squares <- c(1, 201)
+    print(squares)
+  }
+}
+for(which.numbers in numbers){
+  + +     if(sqrt(squared.numbers)==round(sqrt(numbers))){
+    + +         squares <- c(1, 201)
+  }}
+first class exercise
+
